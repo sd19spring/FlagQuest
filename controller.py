82f5f72d595e7fca245a251f39abf5649f88f232
Griffith-Stites <gjstites@gmail.com>
2019-04-10 21:39:52 -0400

    Initialize the controller module

---
+++ controller.py
@@ -0,0 +1,14 @@
+class Player_Controller():
+    """Defines a controller that takes user input to control the Player
+    object.
+    """
+    def __init__(self):
+        """Initialize the player controller"""
+        self.angle = 0 # angle
+        self.v_x = 0 # x velocity
+        self.v_y = 0 # y velocity
+
+class Keyboard_Controller():
+    """Defines a controller that takes input from the arrow keys, wasd, and ,aoe
+    """
+    pass
