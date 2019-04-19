8dad51c539d6357e86bef69a57206ff42b0d0ef9
David Freeman <31573859+DaveFreem@users.noreply.github.com>
2019-04-19 12:08:21 -0400

    Delete view.py

--- view.py
+++
@@ -1,9 +0,0 @@
-import pygame
-import player_actor
-
-class Game_Window(object):
-    def __init__(self, model, x_size, y_size):
-        self.model = model
-        self.x_size = x_size
-        self.y_size = y_size
-        self.screen = pygame
