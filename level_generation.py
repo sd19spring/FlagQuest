56361b32ec4df408e6d77d6597d2b5784b19ea07
Emma Mack <emack@olin.edu>
2019-04-19 12:29:09 -0400

    Add level generation shell

---
+++ level_generation.py
@@ -0,0 +1,6 @@
+#This is empty because we don't know what we're doing yet. Please help.
+
+#A generated level will place obstacles, but be playable (so you can get to all
+#the colors)
+
+#Will use Grid/ Cells in some way.
