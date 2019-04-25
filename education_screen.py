f479d363eb85da8c9a9c99baf49853dc5c0c3701
Griffith-Stites <gjstites@gmail.com>
2019-04-25 14:43:41 -0400

    Add text centering method

--- education_screen.py
+++ education_screen.py
@@ -1,13 +1,6 @@
 from flag import *
 import PIL
 from PIL import Image, ImageDraw
-class Text():
-    def draw_title():
-        pass
-    def draw_description():
-        pass
-    def draw_history():
-        pass
 
 class FinalScreen():
     """
@@ -24,26 +17,24 @@ class FinalScreen():
     def __str__(self):
         return "Final screen for the %s flag of size %s." % (self.flag, self.screen_size)
 
-    def scale_image(self):
-        """Scale the flag image to fit into the top 1/4 of the window"""
-        pass
-
     def get_text(self):
         """Pull the text out of the flag info txt file"""
-        f = open('./flag_info/' + self.flag + '.txt', 'r') # opens the file in read mode
-        lines = f.readlines()
+        filename = './flag_info/' + self.flag + '.txt'
+        with open(filename) as f:
+            lines = f.read().splitlines()
         self.title = lines[0]
         self.description = lines[1]
         self.colors = lines[2]
         self.history = lines[3]
-        print(self.title)
-        print(self.description)
-        print(self.colors)
-        print(self.history)
-        # remove spaces in the name
-    def draw_background():
+        # remove spaces in the name?
+
+    def scale_image(self):
+        """Scale the flag image to fit into the top 1/4 of the window"""
         pass
-        
+
+    def draw_background(self):
+        self.img = Image.new('RGBA', self.screen_size, color = (255, 255, 255)) # create background
+
     def make_flag(self):
         stripe = Image.open('./images/'+ self.flag + '/1.png') # get the first stripe
         self.flag_img = Image.new("RGBA", stripe.size) # make the flag the right dimensions
@@ -58,16 +49,31 @@ class FinalScreen():
                 running = Flase
             n += 1
 
+    def center(self, text, coord):
+        """Center the text around the given coord.
+
+        text: string of text to center
+        coord: tuple of the text center coordinates"""
+        draw = ImageDraw.Draw(self.img)
+        w, h = draw.textsize(text) # get the size of the text given the text size of the image
+        W, H = coord
+        return ((W-w)/2,(H-h)/2)
+
     def draw (self):
         """Draw the final screen"""
-        img = Image.new('RGB', self.screen_size, color = (0, 255, 255)) # create background
-        text = ImageDraw.Draw(img)
-        text.text((10,10), "Hello World", fill=(0,0,0))
+        self.draw_background()
+        # self.img.textsize = 12
+
+        title = ImageDraw.Draw(self.img)
+        position = self.center(self.title, (self.screen_size[0], 30))
+        title.text(position, self.title, fill=(0,0,0))
+        self.img.save('./flag_info/' + self.flag + '_final.png')
+
+        # text.text((1*self.screen_size[0]/2,10), self.text.title, fill=(0,0,0))
+        # enter(self.text.title, (1*self.screen_size[0]/2,10))
         # flag = Image.open('./images/'+ self.flag + '/1.png')
-        self.make_flag()
+        # self.make_flag()
         # img.paste(self.flag_img)
-        img = self.flag_img
-        img.save('./flag_info/' + self.flag + '_final.png')# OPTIMIZE:
 
         # images
         # text
