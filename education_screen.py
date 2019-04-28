838f6b4cf37bee8801591ef5a8c02df84fe89efe
Griffith-Stites <gjstites@gmail.com>
2019-04-27 21:29:16 -0400

    Fix page flipping

--- education_screen.py
+++ education_screen.py
@@ -2,7 +2,7 @@ import pygame
 
 class Page():
     def __init__(self, image_loc, size, page_number):
-        self.image = pygame.transform.scale(pygame.image.load(image_loc), size)
+        self.image = pygame.transform.scale(pygame.image.load(image_loc), size))
         self.page_number = page_number
 
 class Book():
@@ -14,7 +14,7 @@ class Book():
         self.flag = flag_name
         self.size = size
         self.get_pages()
-        self.current_page = self.pages[0]
+        self.current_page = 0
 
     def get_pages(self):
         """Get the pages for the book
@@ -35,11 +35,13 @@ class Book():
     def flip_page(self, direction):
         """Flip the page in the book if there is another page in that direction"""
         # if flipping left and not on the first page
-        if direction == 'left' and self.current_page.page_number > 0:
-            self.current_page += 1
-        # if flipping right and not on the last page
-        elif direction == 'right' and self.current_page.page_number < len(self.pages):
+        print(self.current_page)
+        if direction == 'left' and self.current_page > 0:
             self.current_page += -1
+        # if flipping right and not on the last page
+        elif direction == 'right' and self.current_page < (len(self.pages)-1):
+            self.current_page += 1
+
 class EndScreen():
     def __init__(self, flag_name, screen_size):
         self.flag = flag_name
@@ -47,7 +49,13 @@ class EndScreen():
         self.book = Book(self.flag, self.screen_size) # create a book to fill the screen
 
     def get_keypress(self):
-        self.cont.pressed(pygame.key.get_pressed())
+        self.move_left = [pygame.K_LEFT, pygame.K_a]
+        self.move_right = [pygame.K_RIGHT, pygame.K_d, pygame.K_e]
+        key = pygame.key.get_pressed()
+        if key[self.move_left[0]] == 1 or key[self.move_left[1]] == 1 == 1:
+            self.book.flip_page('left')
+        elif key[self.move_right[0]] == 1 or key[self.move_right[1]] == 1 or key[self.move_right[2]] == 1:
+            self.book.flip_page('right')
 
 if __name__ == '__main__':
     EndScreen('trans', (1920, 1080))
