88b4dfef302d86936d38237f73386986a9bdfc75
Griffith-Stites <gjstites@gmail.com>
2019-04-30 21:57:36 -0400

    Remove player_size and sprint attribute from Player

--- actors.py
+++ actors.py
@@ -63,7 +63,7 @@ class Color(Actor):
 # SCREEN EDGES SHOULD BE HANDLED ELSEWHERE
 # OBSTACLE COLLISION SHOULD BE HANDLED LSEWHERE
 # COLOR COLLISION SHOULD BE HANDLED ELSEWHEER
-class Player(Actor, pygame.sprite.Sprite):
+class Player(Actor):
     """
     The Player class contains methods specific
     to the playable character.
@@ -79,18 +79,16 @@ class Player(Actor, pygame.sprite.Sprite):
         obstacles: brings in a list of all obstacles on map
         """
         super(Player, self).__init__(image, size, pos)
-        pygame.sprite.Sprite.__init__(self) # set up the actor's spriteness
+        # pygame.sprite.Sprite.__init__(self) # set up the actor's spriteness
 
-        # self.image = image
-        # self.image_orig = self.image # sets an original copy of the image to reference later
-        self.player_size = self.image.get_size()    # player_size is a tuple representing the image's dimensions
+        # self.size = self.image.get_size()    # size is a tuple representing the image's dimensions
         self.get_rotations()
         self.position_c = [pos[0] + pos[0]/2, pos[1] + pos[1]/2] # find the center of the image
         self.grid_cell = (numpy.rint(self.position_c[0]/40), numpy.rint(self.position_c[1]/40))
         self.screen_size = screen_size   # refers to screen size
-        # self.rect = pygame.Rect(self.position_c[0] - self.player_size[0]/2,
-        #             self.position_c[1] - self.player_size[1]/2,
-        #             self.player_size[0], self.player_size[1])
+        # self.rect = pygame.Rect(self.position_c[0] - self.size[0]/2,
+        #             self.position_c[1] - self.size[1]/2,
+        #             self.size[0], self.size[1])
 
         self.cont = controller(max_velocity = 10)       # initialize velocity of player
 
@@ -99,7 +97,7 @@ class Player(Actor, pygame.sprite.Sprite):
         self.color_objs = color_objs
 
     def __str__(self):
-        return "Player centered at location %s with a %d-degree heading. The sprite's dimensions are %dx%d" % (self.position_c, self.cont.angle, self.player_size[0], self.player_size[1])
+        return "Player centered at location %s with a %d-degree heading. The sprite's dimensions are %s" % (self.position_c, self.cont.angle, str(self.size))
 
     def get_rotations(self):
         """Get all the rotations for the player image"""
@@ -154,9 +152,9 @@ class Player(Actor, pygame.sprite.Sprite):
     def get_draw_position(self):
         """Finds the position to draw the player at. Based on if moving at a 45 or 90 degree angle"""
         if self.cont.v_x != 0 and self.cont.v_y != 0: # If moving at an angle
-            return (self.position_c[0] - 3*self.player_size[0]/4, self.position_c[1] - 3*self.player_size[0]/4) # translates centered dimensions to account for 45 degree movement
+            return (self.position_c[0] - 3*self.size[0]/4, self.position_c[1] - 3*self.size[0]/4) # translates centered dimensions to account for 45 degree movement
         else: # if standing still or moving at 90 degrees
-            return (self.position_c[0] - self.player_size[0]/2, self.position_c[1] - self.player_size[1]/2)    # translates centered dimensions back to top-left corner dimensions
+            return (self.position_c[0] - self.size[0]/2, self.position_c[1] - self.size[1]/2)    # translates centered dimensions back to top-left corner dimensions
 
     def update_position(self):
         """Update the image based on the facing of the player"""
@@ -170,9 +168,9 @@ class Player(Actor, pygame.sprite.Sprite):
 
     def update_rect(self):
         """Updates rect boundary, for use in collision detection"""
-        self.rect = pygame.Rect(self.position_c[0] - self.player_size[0]/2,
-                    self.position_c[1] - self.player_size[1]/2,
-                    self.player_size[0], self.player_size[1])
+        self.rect = pygame.Rect(self.position_c[0] - self.size[0]/2,
+                    self.position_c[1] - self.size[1]/2,
+                    self.size[0], self.size[1])
 
     def check_obstacle_collision(self):
         """Returns sprite collided with, of obstacle objects, or None if no collisions."""
