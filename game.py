f0ce9add52d5a82748a84162d6798a33dda7ef70
Griffith-Stites <gjstites@gmail.com>
2019-05-01 19:04:52 -0400

    Remove collision class

--- game.py
+++ game.py
@@ -17,7 +17,6 @@ class Game():
         self.fill_color = fill
         self.model = gameworld.Model()
         self.view = gameworld.View(size, self.fill_color, self.model)
-        self.collision = gameworld.Collision(self.model)
         self.running = True
 
     def check_events(self):
@@ -41,9 +40,6 @@ class Game():
                 self.model.make_endscreen()
                 self.model.endgame = True
                 self.fill_color=(255, 255, 255)
-        # MOVE THIS TO COLLISION CLASS
-        self.collision.update()
-
 
     def update(self):
         """Updates the game for one tick"""
