4f7d6b5983fbe3ea3d1d76a63866bdbe3376a938
Griffith-Stites <gjstites@gmail.com>
2019-05-02 17:06:53 -0400

    Clean up game.py

--- game.py
+++ game.py
@@ -13,7 +13,6 @@ class Game():
 
         size: Tuple of the window dimensions
         fill: Tuple of the RGB color"""
-        # add file image?
         self.fill_color = fill
         self.model = gameworld.Model()
         self.view = gameworld.View(size, self.fill_color, self.model)
@@ -31,7 +30,6 @@ class Game():
                 pygame.display.set_mode((1880, 1080), pygame.RESIZABLE)
 
     def check_collision(self):
-        # touched_piece is a piece of the flag that the player just collided with (if they even did)
         touched_piece = self.model.player.check_color_collision(self.model.color_objs)
         if touched_piece:
             touched_piece.exists = False # makes the touched piece disappear
@@ -40,9 +38,6 @@ class Game():
                 self.model.make_endscreen()
                 self.model.endgame = True
                 self.fill_color=(255, 255, 255)
-        # MOVE THIS TO COLLISION CLASS
-        # self.collision.update()
-
 
     def update(self):
         """Updates the game for one tick"""
